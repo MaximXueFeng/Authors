09a201b25682f660b23c67f54ce84b64440547d9
MaximXueFeng <mspashinskiy@gmaol.com>
2021-04-11 13:24:43 +0300

    Added brackets, removed SQL injection

--- authors_books.rb
+++ authors_books.rb
@@ -13,18 +13,18 @@ get '/' do
 end
 
 get '/authors/:id' do
-  @author = connection.exec_params("SELECT authors.* FROM authors WHERE authors.id = $1::int", params[:id]).first
+  @author = connection.exec_params("SELECT authors.* FROM authors WHERE authors.id = $1::int", [params[:id]]).first
   @books = connection.exec_params("SELECT books.* FROM books
   JOIN authors_books ON books.id = authors_books.book_id
-  WHERE authors_books.author_id = $1::int", params[:id])
+  WHERE authors_books.author_id = $1::int", [params[:id]])
 
   erb :author
 end
 
 get '/books/:id' do
-  @book = connection.exec_params("SELECT books.* FROM books WHERE books.id = $1::int", params[:id]).first
+  @book = connection.exec_params("SELECT books.* FROM books WHERE books.id = $1::int", [params[:id]]).first
   @screen_adaptations = connection.exec_params("SELECT screen_adaptations.* FROM screen_adaptations
-  WHERE screen_adaptations.book_id = $1::int", params[:id])
+  WHERE screen_adaptations.book_id = $1::int", [params[:id]])
 
   erb :book
 end
